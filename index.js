2acd0c0667a110d2939921ea9f8b5aca09d151c8
Elina Satski <elina.satski@tptlive.ee>
2025-09-22 15:21:01 +0300

    let example

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-var foo;
-var bar;
-
-console.log(foo);
-console.log(bar);
-
-foo = 'Hello'; // old way don't use!
-bar = 'World';
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num =1.2;
+console.log(num);
+console.log(typeof num);
+let text = 'Hello world';
+console.log(text);
+console.log(typeof text);
+text = 'H'; //char
+console.log(text);
+console.log(typeof text);
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+let array =[1, 2, 'hello', 'world', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+let obj = {
+    name: 'Elina',
+    age: 18,
+    hairColor: 'Brown',
+    isFemale: true,
+};
+console.log(obj);
+console.log(typeof obj);
